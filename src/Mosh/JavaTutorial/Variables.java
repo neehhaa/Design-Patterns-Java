e959b15bb79d075e65e69bda964cf5331b39594c
neehhaa <nehawasnik95@outlook.com>
2022-10-31 16:46:57 +0530

    Variables

---
+++ src/Mosh/JavaTutorial/Variables.java
@@ -0,0 +1,13 @@
+package Mosh.JavaTutorial;
+
+public class Variables {
+    public static void main(String[] args) {
+        int age = 30;
+        age = 35;
+        System.out.println(age);
+
+        int myAge = 27;
+        int herAge = myAge;
+        System.out.println(herAge);
+    }
+}
