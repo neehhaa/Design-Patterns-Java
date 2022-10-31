b7baf0b0bf64f5212324bdbc642a309c513faf32
neehhaa <nehawasnik95@outlook.com>
2022-10-31 17:07:16 +0530

    Reference Types

---
+++ src/Mosh/JavaTutorial/ReferenceTypes.java
@@ -0,0 +1,10 @@
+package Mosh.JavaTutorial;
+
+import java.util.Date;
+
+public class ReferenceTypes {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println(now);
+    }
+}
