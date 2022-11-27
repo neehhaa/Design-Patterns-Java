175fb6342f3a2145e7a92ab05439ab76b385723a
neehhaa <nehawasnik95@outlook.com>
2022-11-27 15:32:15 +0530

    Abstract class

---
+++ src/DerekBanas/AbstractClasses/WorkWithAnimals.java
@@ -0,0 +1,9 @@
+package DerekBanas.AbstractClasses;
+
+public class WorkWithAnimals {
+    public static void main(String[] args) {
+        Giraffe giraffe = new Giraffe();
+        giraffe.setName("Frank");
+        System.out.println(giraffe.getName());
+    }
+}
