ed8b940fdee8cc7ad20c775fa70debc58a8af08c
neehhaa <nehawasnik95@outlook.com>
2022-11-27 17:52:01 +0530

    Interface class

---
+++ src/DerekBanas/InterfaceClasses/Living.java
@@ -0,0 +1,21 @@
+package DerekBanas.InterfaceClasses;
+
+public interface Living {
+    public void setName(String newName);
+    public String getName();
+
+    public void setHeight(double newHeight);
+    public double getHeight();
+
+    public void setWeight(double newWeight);
+    public double getWeight();
+
+    public void setFavFood(double newFavFood);
+    public double getFavFood();
+
+    public void setSpeed(double newSpeed);
+    public double getSpeed();
+
+    public void setSound(double newSound);
+    public double getSound();
+}
---
+++ src/DerekBanas/InterfaceClasses/Monkey.java
@@ -0,0 +1,63 @@
+package DerekBanas.InterfaceClasses;
+
+public class Monkey implements Living{
+    @Override
+    public void setName(String newName) {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void setHeight(double newHeight) {
+
+    }
+
+    @Override
+    public double getHeight() {
+        return 0;
+    }
+
+    @Override
+    public void setWeight(double newWeight) {
+
+    }
+
+    @Override
+    public double getWeight() {
+        return 0;
+    }
+
+    @Override
+    public void setFavFood(double newFavFood) {
+
+    }
+
+    @Override
+    public double getFavFood() {
+        return 0;
+    }
+
+    @Override
+    public void setSpeed(double newSpeed) {
+
+    }
+
+    @Override
+    public double getSpeed() {
+        return 0;
+    }
+
+    @Override
+    public void setSound(double newSound) {
+
+    }
+
+    @Override
+    public double getSound() {
+        return 0;
+    }
+}
